af3fa0dcb9d7c103cdea1f1745deb4fda4f1f2af
sven-mycrocast <67374475+sven-mycrocast@users.noreply.github.com>
2024-01-03 11:37:36 +0100

    renamend

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "MCCOpus", path: "frameworks/libopus.xcframework"),
+        .binaryTarget(name: "MCCOpus", path: "frameworks/MCCOpus.xcframework"),
     ]
 )
